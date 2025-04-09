52bc6d2e13633e13e634eab125a18e5f2e578277
goat-helloworldlabs <goat.ohgiraffers@gmail.com>
2025-04-09 13:27:59 +0900

    db.json 위치 변경

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const jsonServer = require('json-server');
-const path = require('path');
-
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db', 'db.json'));
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -10,5 +8,5 @@ server.use(router);
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log(`🚀 JSON Server is running on port ${port}`);
+  console.log(`JSON Server is running on port ${port}`);
 });
